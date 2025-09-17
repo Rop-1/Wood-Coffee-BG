62377af7e7be5c5480e6aefb7aa57bb49896d515
R0p-1 <varga.gergo.2023i@bankitatabanya.hu>
2025-09-17 19:31:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Wood-Coffee-BG" 
---
+++ js/slider.js
@@ -0,0 +1,19 @@
+document.getElementById('load-more-btn').addEventListener('click', function() {
+    const extraProducts = document.getElementById('extra-products');
+    const button = document.getElementById('load-more-btn');
+    if (extraProducts.style.display === 'none') {
+        extraProducts.style.display = 'flex';
+        button.innerText = 'További termékek elrejtése';
+    } else {
+        extraProducts.style.display = 'none';
+        button.innerText = 'További termékek megtekintése';
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+});
+
+function responsive() {
+  document.getElementById("NavBar").classList.toggle("responsive");
+}
